ea9ee217b5abe32609ba5bc9c2138af759835c4c
delehei6309 <wangchunting7@163.com>
2017-12-20 00:20:53 +0800

    create navigation

---
+++ src/components/Navigation/Navigation.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="navigation">
+        navigation
+    </div>
+</template>
+
+<script>
+    import './navigation.less';
+    export default {
+        name: 'navigation',
+        data(){
+            return {}
+        },
+        created(){
+        },
+        computed: {},
+        methods: {},
+        destroyed(){
+
+        }
+    }
+</script>
--- src/containers/Solution.vue
+++ src/containers/Solution.vue
@@ -1,31 +1,35 @@
 <template>
     <div class="solution">
-        
+
         解决方案
+        <navigation></navigation>
     </div>
 </template>
 
 <script>
     import $api from '../tools/api';
     import '../less/solution.less';
+    import Navigation from '../components/Navigation';
     export default {
         name: 'solution',
         data(){
             return {
-                
+                template:''
+
             }
         },
         created(){
-           
+
         },
         components: {
+            Navigation
         },
         computed: {},
         methods: {
-            
+
         },
         watch: {
-            
+
         },
         destroyed(){
 
