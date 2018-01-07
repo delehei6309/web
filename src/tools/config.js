88e5747ccb715e58e725df523fef4834a8cb179c
delehei6309 <wangchunting7@163.com>
2018-01-08 00:43:28 +0800

    merge dev

--- src/tools/config.js
+++ src/tools/config.js
@@ -4,7 +4,7 @@
 let protocol = window.location.protocol;
 console.log(protocol)
 export const devUrl = `${protocol}//localhost/company/public`;
-export const testUrl = `${protocol}//localhost/company/public`;
+export const testUrl = `${protocol}//127.0.0.1`;
 export const productionUrl = `${protocol}//127.0.0.1`;
 export const telNumber = '400-100-1000';
 console.log(devUrl);
