001165918f953099541699fd8cd82a9304a79c87
二三子 <wangchunting7@163.com>
2018-01-17 10:01:36 +0800

    config

--- src/tools/config.js
+++ src/tools/config.js
@@ -5,7 +5,7 @@ let protocol = window.location.protocol;
 console.log(protocol)
 export const devUrl = `${protocol}//localhost/company/public`;
 export const testUrl = `${protocol}//127.0.0.1`;
-export const productionUrl = `${protocol}//zhiyuan.com`;
+export const productionUrl = `${protocol}//www.zhiyuanjiahe.com`;
 console.log(devUrl);
 export const sysDev = '';
 export const sysTest = '';
