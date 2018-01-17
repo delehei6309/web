621503601820094f23e51b5ef01de31b6b8a3b4c
二三子 <wangchunting7@163.com>
2018-01-17 10:04:41 +0800

    merge build

--- src/tools/config.js
+++ src/tools/config.js
@@ -6,7 +6,6 @@ console.log(protocol)
 export const devUrl = `${protocol}//localhost/company/public`;
 export const testUrl = `${protocol}//127.0.0.1`;
 export const productionUrl = `${protocol}//www.zhiyuanjiahe.com`;
-console.log(devUrl);
 export const sysDev = '';
 export const sysTest = '';
 export const sysProduction = ``;
