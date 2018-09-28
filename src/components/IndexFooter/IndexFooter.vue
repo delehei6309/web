f9d586d0bc461744bf312ec05658280d8c14df13
delehei6309 <wangchunting7@163.com>
2018-09-28 10:14:22 +0800

    修改公司地址

--- src/components/IndexFooter/IndexFooter.vue
+++ src/components/IndexFooter/IndexFooter.vue
@@ -2,7 +2,7 @@
     <div class="index-footer">
         <div class="footer-content clear">
             <div class="div-left">
-                <p>地址：北京市海淀区双榆树东里甲22号都市网景2号楼508室</p>
+                <p>地址：北京市海淀区学清路16号学知轩大厦1812室</p>
                 <p>电话：010-62155676&nbsp;&nbsp;传真：010-62155626</p>
                 <p>QQ：76431061943672997&nbsp;&nbsp;Email：bjwit2007@163.com</p>
                 <!--<p>Copyright © 2002-2017 北京致远嘉禾科技发展有限公司 版权所有 ICP备案:京ICP备14040948号-1</p>-->
